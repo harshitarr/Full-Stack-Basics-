49d4e1e0356de5c6480802a3eea225534848086b
Harshita <harshitarera@gmail.com>
2025-01-17 09:16:16 +0530

    commit

---
+++ BasicOpeators/IfElse.js
@@ -0,0 +1,9 @@
+const saraage = 16
+if(saraage >= 18)
+{
+    console.log("Sara is ready to get the lisense")
+}
+
+else{
+    console.log("Sara need to wait for " + (18-saraage) +" more year to get the lisense")
+}
---
+++ BasicOpeators/String.js
@@ -0,0 +1,11 @@
+const firstname = 'jonas'
+
+const job = 'teacher'
+const birthyear = 1992
+const year = 2017
+
+const name ="I'm" + firstname +", a" + (year-birthyear) + "year old"+job+"!"
+console.log(name)
+
+const name2 = `I'm ${firstname} , a ${year-birthyear} year old ${job} !`
+console.log(name2)
