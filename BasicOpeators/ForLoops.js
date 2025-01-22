277f5fd336ae865391c205dc0d8b9597e7f0cd4e
Harshita <harshitarera@gmail.com>
2025-01-23 00:32:10 +0530

    commit

---
+++ BasicOpeators/ForLoops.js
@@ -0,0 +1,8 @@
+const jonas = [
+'jonas','schmiden',67 , 'teacher' , ['michale','peter','steven']
+];
+
+for(let i = jonas.length-1; i >= 0 ;i-- )
+{
+ console.log(jonas[i])
+}
---
+++ BasicOpeators/whileloop.js
@@ -0,0 +1,9 @@
+
+let dice = Math.trunc(Math.random()*6)+1;
+
+while (dice!==6)
+{
+    console.log(`your dice ${dice}`);
+    dice = Math.trunc(Math.random()*6)+1
+ 
+}
