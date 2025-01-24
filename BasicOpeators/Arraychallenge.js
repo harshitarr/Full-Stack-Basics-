7ed407e78dd2e7b5085d8fd77a02bc1381b3bb2a
Harshita <harshitarera@gmail.com>
2025-01-24 23:09:17 +0530

    commit

---
+++ BasicOpeators/Arraychallenge.js
@@ -0,0 +1,28 @@
+// sample data set 1
+
+const arr = [17,21,23]
+
+const forcastTemp = function(arr)
+{
+   for(let i = 0 ; i<arr.length;i++)
+   {
+    console.log(`${arr[i]} in ${i+1} days`)
+   }
+}
+
+forcastTemp(arr)
+
+console.log("---------------")
+// sample data set 2
+
+const arr2 = [12,5,-5,0,4]
+
+const forcastTemp2 = function(arr2)
+{
+   for(let i = 0 ; i<arr2.length;i++)
+   {
+    console.log(`${arr2[i]} in ${i+1} days`)
+   }
+}
+
+forcastTemp2(arr2)
