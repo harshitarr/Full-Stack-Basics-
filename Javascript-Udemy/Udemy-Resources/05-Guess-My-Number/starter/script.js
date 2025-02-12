09020d8e336abfb4923c82a859796f77d877cb45
Harshita <harshitarera@gmail.com>
2025-02-12 23:11:33 +0530

    Commit

---
+++ Javascript-Udemy/Udemy-Resources/05-Guess-My-Number/starter/script.js
@@ -0,0 +1,53 @@
+document.querySelector('.message').textContent = "Awesome buddy !"
+const number = Math.trunc(Math.random()*20)+1
+console.log(number)
+
+let score = Number(document.querySelector('.score').textContent)
+console.log(score)
+document.querySelector('.check').addEventListener('click',function()
+{
+    console.log(document.querySelector('.guess').value)
+    guess =Number( document.querySelector('.guess').value)
+if(!guess)
+{
+   document.querySelector('.message').textContent = "Omg There is none !"
+}
+
+else if(guess === number)
+{
+    document.querySelector('.message').textContent = "Gotcha !"
+}
+
+else if(guess < number)
+    {
+        if(score > 0)
+        {
+
+      
+        document.querySelector('.message').textContent = "Too Small"
+        score = score - 1
+        document.querySelector('.score').textContent = score
+       }
+       else
+       {
+        document.querySelector('.message').textContent = "Game Over"
+       }
+    }
+
+    else if(guess > number)
+        {
+            if(score>0)
+            {
+            document.querySelector('.message').textContent = "Too Big"
+            score = score - 1
+            document.querySelector('.score').textContent = score
+            }
+
+            else{
+                document.querySelector('.message').textContent = "Game Over"
+
+            }
+        }
+
+
+})
