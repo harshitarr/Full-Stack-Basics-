c8d23f289198d9eff7762a12134b5d46493f8232
Harshita <harshitarera@gmail.com>
2025-02-14 23:38:55 +0530

    m

---
+++ Javascript-Udemy/Udemy-miniprojects/06-Modal/starter/script.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnclose = document.querySelector('.close-modal')
+const btnopen = document.querySelectorAll('.show-modal')
+
+
+for(let i=0 ; i < btnopen.length ; i++)   // this loop will run through all the show modal buttons
+
+    btnopen[i].addEventListener('click',function(){
+
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
+
+btnclose.addEventListener('click',function(){
+
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+
+})
+
