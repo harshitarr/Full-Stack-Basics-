117537bc1f327e2690a914036cc328bf38152895
Harshita <harshitarera@gmail.com>
2025-02-19 10:29:15 +0530

    commit

--- BasicOpeators/arrays/arrays1.js
+++ BasicOpeators/arrays/arrays1.js
@@ -5,19 +5,25 @@ const restaurant = {
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-    order: function (starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    order: function(starterindex , mainindex)
+    {
+        return[this.starterMenu[starterindex],this.mainMenu[mainindex]]
     }
-};
+    };
+
 
 const arr = [1, 2, 3];
 const [x, y, z] = arr;
 console.log(x, y, z);
 
-let [main, secondary] = restaurant.categories;
-console.log(main, secondary);
+let [first,second] = restaurant.categories;
 
+[first,second]=[second,first];
 
-[main, secondary] = [secondary, main];
+console.log(first,second);
 
-console.log(main, secondary);
+const [starter , main] = restaurant.order(2,0)
+
+console.log(starter,main) // note the difference in o/p
+
+console.log(restaurant.order(2,0))// check the diff
