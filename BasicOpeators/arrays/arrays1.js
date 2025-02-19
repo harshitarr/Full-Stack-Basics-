bc8804dca3100daf1c8898b119137a3ae3da843d
Harshita <harshitarera@gmail.com>
2025-02-19 09:52:08 +0530

    commit

--- BasicOpeators/arrays/arrays1.js
+++ BasicOpeators/arrays/arrays1.js
@@ -1,7 +1,23 @@
-const arr = [1 , 2 , 3]
-const a = arr[0]
-const b = arr[1]
-const c = arr[2]
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-const [x , y , z] = arr
-console.log(x , y, z)
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
+};
+
+const arr = [1, 2, 3];
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+
+[main, secondary] = [secondary, main];
+
+console.log(main, secondary);
