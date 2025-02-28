9d856f489797c63db9d157c2f32957e3c9984cdc
Harshita <harshitarera@gmail.com>
2025-02-28 11:19:52 +0530

    coomi

--- BasicOpeators/Functions/moreAbtClosure.js
+++ BasicOpeators/Functions/moreAbtClosure.js
@@ -23,5 +23,26 @@ const h = function()
 
 g()
 f()
+console.dir(f)
+
+// reassigning f value
 h()
 f()
+console.dir(f)
+
+
+const boardPassengers = function(n , wait){
+
+    const perGroup = n/3
+
+    setTimeout(function(){
+
+        console.log(`we are now boarding all ${n} passenger`)
+
+        console.log(`there are 3 groups , each with ${perGroup} passengers`)
+    }, wait*1000)
+
+    console.log(`Will start boarding in ${wait} seconds`)
+}
+
+boardPassengers(180,3)
