625d2e0aafa63aff99c05ec874c27b6ecbe6a524
Harshita <harshitarera@gmail.com>
2025-03-03 15:50:39 +0530

    commit

--- Javascript-Udemy/Udemy-miniprojects/11-Arrays-Bankist/starter/script.js
+++ Javascript-Udemy/Udemy-miniprojects/11-Arrays-Bankist/starter/script.js
@@ -88,6 +88,31 @@ const callDisplayBalance = function(movements){
 }
 
 callDisplayBalance(account1.movements)
+
+
+
+
+
+const calDisplaySummary = function(movements){
+
+  const incomes = movements.filter(mov => mov > 0).reduce((acc , mov) => acc + mov , 0)
+  labelSumIn.textContent = `${incomes}`
+
+  const out = movements.filter(mov => mov < 0).reduce((acc , mov) => acc + mov , 0)
+  labelSumOut.textContent = `${Math.abs(out)}`
+
+  const interest = movements.filter(mov => mov > 0).map(deposit => deposit * 1.2 / 100).filter((int , i , arr) =>{
+    return int >= 1
+  } ).reduce((acc , int) => acc + int , 0)
+  labelSumInterest.textContent = `${interest}`
+}
+
+calDisplaySummary(account1.movements)
+
+
+
+
+
 const createUserName = function(accs) {
 
   accs.forEach(function(acc){
