6c948403ed07f1a695f6c28d9af022885ab99eee
Harshita <harshitarera@gmail.com>
2025-03-03 15:30:46 +0530

    cmt

--- Javascript-Udemy/Udemy-miniprojects/11-Arrays-Bankist/starter/script.js
+++ Javascript-Udemy/Udemy-miniprojects/11-Arrays-Bankist/starter/script.js
@@ -124,7 +124,7 @@ createUserName(accounts)
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const eurToUsd = 1.1
+
 const movementsUSD = movements.map(function(mov){
 
   return mov * eurToUsd
@@ -146,3 +146,17 @@ console.log(movementsUSD)
  })
 
  console.log(movementsDescriptions)
+
+ const eurToUsd = 1.1
+
+ console.log(movements)
+
+
+ const totalDepositsUsd = movements.filter(mov => mov > 0).map((mov , i , arr) => {
+
+  console.log(arr)
+  return mov * eurToUsd
+ })
+
+ .reduce((acc,mov) => acc + mov , 0)
+ console.log(totalDepositsUSD)
