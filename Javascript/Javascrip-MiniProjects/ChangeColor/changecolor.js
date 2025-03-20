0ab2b3cde085251a9243288ce788da6563afc332
Harshita <harshitarera@gmail.com>
2025-03-20 10:51:09 +0530

    changecolour

--- Javascript/Javascrip-MiniProjects/ChangeColor/changecolor.js
+++ Javascript/Javascrip-MiniProjects/ChangeColor/changecolor.js
@@ -1,1 +1,19 @@
-let
+const color = ["#b78fcb","#cb99c7","#efbbcd","#ffcbd8","#c3adc3","#bd959f","#eeb5b5"]
+
+let buttonclick = document.querySelector(".button")
+let bgcolour = document.querySelector("body")
+let colourtext = document.querySelector(".colortext")
+
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*color.length)
+}
+
+buttonclick.addEventListener("click",function(){
+
+    const randomNo = getRandomNumber()
+    bgcolour.style.backgroundColor=color[randomNo]
+    colourtext.textContent=color[randomNo]
+
+})
+
